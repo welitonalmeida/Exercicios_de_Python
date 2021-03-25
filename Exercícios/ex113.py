ee259ed819f5ea06f608d59e17ee192a29506610
Weliton de Almeida <79901750+welitonalmeida@users.noreply.github.com>
2021-03-24 23:52:03 -0300

    Adição de exercícios

---
+++ Exercícios/ex113.py
@@ -0,0 +1,31 @@
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número inteiro válido!!!\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        else:
+            return n
+
+
+def leiafloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número Real válido!!!\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        else:
+            return n
+
+            
+n1 = leiaint('Digite um valor Inteiro: ')
+n2 = leiafloat('Digite um valor Real: ')
+print(f'O valor inteiro digitado foi {n1} e o real foi {n2}.')
