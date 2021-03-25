851bccb3c9047e9387cf9772d313374cb165b059
Weliton de Almeida <79901750+welitonalmeida@users.noreply.github.com>
2021-03-25 02:58:25 -0300

    Update de exercícios

--- Exercícios/ex115/sistema.py
+++ Exercícios/ex115/sistema.py
@@ -2,7 +2,7 @@ from lib.interface import *
 from lib.arquivo import *
 from time import sleep
 
-arq = 'curso.txt'
+arq = 'dados.txt'
 
 if not arquivoexiste(arq):
     criararquivo(arq)
@@ -13,7 +13,11 @@ while True:
         # LISTAR O CONTEÚDO DE UM ARQUIVO!
         lerarquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        # OPÇÃO PARA CADASTRO DE NOVA PESSOA!
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         sleep(1)
         cabeçalho('Saindo do Sistema... Até logo!')
