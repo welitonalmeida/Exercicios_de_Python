7f0412c57dad9d0a16ce50dd40b7cf8f7d755c1a
Weliton de Almeida <weliton.2020007512@aluno.iffar.edu.br>
2021-12-08 09:11:52 -0300

    Update ex087.py

--- Exercícios/ex087.py
+++ Exercícios/ex087.py
@@ -14,7 +14,7 @@ print('-='*15)
 print(f'A soma dos valores pares é {spar}')
 for l in range(0, 3):
     scol += matriz[l][2]
-print(f'A soma dos valore da terceira coluna é {scol}')
+print(f'A soma dos valores da terceira coluna é {scol}')
 for c in range(0, 3):
     if c == 0:
         mai = matriz[1][c]
