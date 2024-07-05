9950ce01fa1eb04bd25e598e7f7e8577fc0fdc0f
Weliton de Almeida <79901750+welitonalmeida@users.noreply.github.com>
2024-07-05 19:57:16 -0300

    up

--- Exercícios/speedtest.py
+++ Exercícios/speedtest.py
@@ -4,4 +4,5 @@ import speedtest
 teste = speedtest.teste()
 down = teste.download()
 upl = teste.upload()
-print(f"A velocidade de Download é: {down}.\nA velocide de Upload é {upl}.")
+print(f"A velocidade de Download é: {down}.\nA velocidade de Upload é {upl}.")
+
